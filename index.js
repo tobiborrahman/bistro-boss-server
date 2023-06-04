2032613d42e7e44da2aa7d45b76d3395408a7614
TobiborRahman <r.tobibor16@gmail.com>
2023-06-04 12:31:28 +0600

    vercel added

--- index.js
+++ index.js
@@ -133,16 +133,16 @@ async function run() {
 			res.send(result);
 		});
 
-		// review related apis
-		app.get('/reviews', async (req, res) => {
-			const result = await reviewCollection.find().toArray();
-			res.send(result);
-		});
-
 		app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await menuCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		// review related apis
+		app.get('/reviews', async (req, res) => {
+			const result = await reviewCollection.find().toArray();
 			res.send(result);
 		});
 
